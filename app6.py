4703f6523270e12f329db60f17135e844c820fb3
hallerbracho <habracho@fec.luz.edu.ve>
2025-04-20 16:41:29 -0400

    Update app6.py

--- app6.py
+++ app6.py
@@ -50,7 +50,7 @@ model = genai.GenerativeModel(
 def generate_crypto_question_google():
     """Llama a la API de Google Gemini para generar una pregunta con explicación."""
     prompt = f"""
-        Genera una única pregunta de quiz sobre la asignatura Fundamentos de la teoría de conjuntos para estudiantes de computación.
+        Genera una única pregunta de quiz sobre la asignatura Fundamentos de la teoría de conjuntos para estudiantes de ciencias de la computación.
         La pregunta debe venir acompañada con un escenario teórico (máximo 1-2 párrafos) relacionado con la pregunta. 
         La pregunta debe ser de tipo selección múltiple ('mc') con 5 opciones.
         Responde ÚNICAMENTE con un objeto JSON válido que se adhiera ESTRICTAMENTE al siguiente formato, sin texto adicional antes o después del JSON.
